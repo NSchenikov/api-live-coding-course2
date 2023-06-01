bf6bec0f4e19919324155f7d2f6df3c77633e6e4
Nikita-Schenikov <nikita-shenikov@mail.ru>
2023-06-02 01:21:03 +0300

    put log in form to another component

--- script.js
+++ script.js
@@ -4,10 +4,12 @@
 //* сделать форму входа динамической (+)
 //* отрефакторить приложение на модули
 //  *API (+)
-//  *... TODO
+//  *вытащить логин компонент в отдельный модуль (+)
+//  *вытащить компонент списка задач и форму добавления в отдельный модуль (+)
 // 2. Реализовать форму регистрации
 
 import { addTodo, deleteTodo, getTodos } from "./api.js";
+import { renderLoginComponent } from "./components/login-component.js";
 
 //https://github.com/GlebkaF/webdev-hw-api/blob/main/pages/api/v2/todos/README.md  - документация по конкретному api
 
@@ -31,34 +33,12 @@ const renderApp = () => {
   const appEl = document.getElementById('app');
 
   if(!token) {
-    const appHtml = `
-    <h1>Список задач</h1>
-    <div class="form">
-      <h3 class="form-title">Форма входа</h3>
-      <div class="form-row">
-        Логин:
-        <input
-          type="text"
-          id="login-input"
-          class="input"
-        />
-        <br />
-        Пароль:
-        <input
-          type="text"
-          id="login-input"
-          class="input"
-        />
-      </div>
-      <br />
-      <button class="button" id="login-button">Войти</button>
-    </div>`;
-    appEl.innerHTML = appHtml;
 
-    document.getElementById('login-button').addEventListener('click', () => {
-      token = 'Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k'; 
-      fetchTodosAndRender();
-    });
+    renderLoginComponent({ appEl, setToken: (newToken) => {
+      token = newToken;
+    },
+    fetchTodosAndRender,
+  });
 
     return;
   }
