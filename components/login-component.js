f0fd2b8955c34d940f0629712302ac993a1f5e04
Nikita-Schenikov <nikita-shenikov@mail.ru>
2023-06-02 01:42:54 +0300

    add call API of log in

--- components/login-component.js
+++ components/login-component.js
@@ -1,3 +1,5 @@
+import { login } from "../api.js";
+
 export function renderLoginComponent({ appEl, setToken, fetchTodosAndRender }) {
     const appHtml = `
     <h1>Список задач</h1>
@@ -27,10 +29,14 @@ export function renderLoginComponent({ appEl, setToken, fetchTodosAndRender }) {
 
         setToken('Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k');
 
-      fetchTodosAndRender();
-    });
+        login({
+            login: 'admin',
+            password: 'admin',
+        }).then((user) => {
+            console.log(user);
 
-    // renderLoginComponent();
-
-    // return;
+            setToken(`Bearer ${user.user.token}`);
+            fetchTodosAndRender();
+        });
+     });
 }
